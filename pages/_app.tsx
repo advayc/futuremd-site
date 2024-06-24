564dd31ec901bdb20963cf72742bb66ddb40cc2d
Advay Chandorkar <advay.chandorkar@gmail.com>
2024-06-24 01:36:55 -0400

    this works

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { Analytics } from "@vercel/analytics/react"
-
-export default function App({ Component, pageProps }: AppProps) {
-    <Analytics />
-  return <Component {...pageProps} />;
+import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
+ 
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
+ 
+export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import { Analytics } from "@vercel/analytics/react"
+
 export default function Document() {
   return (
     <Html lang="en">
@@ -7,8 +8,8 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Analytics />
       </body>
-      <Analytics />
     </Html>
   );
 }
