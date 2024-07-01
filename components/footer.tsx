ddc929ca6758b98c81637e17cf8854247a72cad2
Advay Chandorkar <advay.chandorkar@gmail.com>
2024-07-01 02:30:36 -0400

    issues resolved back to hard reset

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,19 +1,11 @@
-import React from 'react';
-import { FaInstagram, FaLinkedin } from 'react-icons/fa';
 
-const Footer: React.FC = () => {
-    return (
-        <footer className="fixed bottom-0 left-0 right-0 z-10 bg-[#D9EAFF] py-6 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
-            <div className="flex space-x-6 ml-auto">
-                <a href="https://www.instagram.com/futuremd_team/" target="_blank" rel="noopener noreferrer">
-                    <FaInstagram size={40} className="text-blue-600 hover:text-blue-900 transition-all duration-300" />
-                </a>
-                <a href="https://www.linkedin.com/company/futuremdteam/" target="_blank" rel="noopener noreferrer">
-                    <FaLinkedin size={40} className="text-blue-600 hover:text-blue-900 transition-all duration-300" />
-                </a>
-            </div>
-        </footer>
-    );
+export const Footer = () => {
+  return (
+    <div className="px-4 py-2 pb-12 mt-10 rounded-lg flex flex-col md:items-center md:justify-center md:text-center pl-10 md:pl-0">
+      <div className="text-gray-300 text-sm uppercase mb-3">
+        Copyright © {new Date().getFullYear()} ⎯{" "}
+        <span className="text-gray-400 font-bold">Future MD </span>
+      </div>
+    </div>
+  );
 };
-
-export default Footer;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 
 const Navbar: React.FC = () => {
-return (
-    <header className="flex justify-between items-center">
-        <a href="/"><img src="logo.png" alt="Logo" className="h-[50px]"/></a>
-        <nav className="flex items-center">
-            <a href="#" className="text-black font-bold mr-9 hover:text-[#3C55B7] transition duration-200">Our Team</a>
-            <a href="#" className="text-black font-bold mr-9 hover:text-[#3C55B7] transition duration-200">Our Mission</a>
-            <a href="#" className="text-black font-bold mr-9 hover:text-[#3C55B7] transition duration-200">Events</a>
-            <a href="#" className="bg-[#D9EAFF] text-black px-5 py-3 rounded-[10px] font-bold mr-10 hover:opacity-80 transition duration-300">Join Us</a>
-            <a href="/contact" className="bg-[#28387D] text-white px-5 py-3 rounded-[10px] font-bold mr-10 hover:opacity-80 transition duration-300">Contact Us</a>
-        </nav>
+  return (
+    <header className="">
+      <nav className="flex items-center justify-center">
+        <a href="/">
+          <img src="logo.png" alt="Logo" className="h-[50px] mr-9 logo"/>
+        </a>
+        <a href="#" className="text-gray-100 mr-7 hover:text-[#5C9CFF] font-bold transition delay-50">About</a>
+        <a href="#" className="text-gray-100 mr-7 hover:text-[#5C9CFF] font-bold transition delay-50">Sponsors</a>
+        <a href="#" className="text-gray-100 mr-7 hover:text-[#5C9CFF] font-bold transition delay-50">Schedule</a>
+        <a href="#" className="text-gray-100 mr-7 hover:text-[#5C9CFF] font-bold transition delay-50">Media</a>
+        <a href="#" className="text-gray-100 mr-7 hover:text-[#5C9CFF] font-bold transition delay-50">Contact Us</a>
+      </nav>
     </header>
-);
+  );
 };
 
 export default Navbar;
