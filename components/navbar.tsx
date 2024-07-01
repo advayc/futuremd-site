5c585a42676f937f3181ac7a5fdac4e235e32310
Advay Chandorkar <advay.chandorkar@gmail.com>
2024-07-01 03:04:43 -0400

    homepage

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 const Navbar: React.FC = () => {
   return (
-    <header className="">
+    <header className=''>
       <nav className="flex items-center justify-center">
         <a href="/">
           <img src="logo.png" alt="Logo" className="h-[50px] mr-9 logo"/>
         </a>
-        <a href="#" className="text-gray-100 mr-7 hover:text-[#5C9CFF] font-bold transition delay-50">About</a>
-        <a href="#" className="text-gray-100 mr-7 hover:text-[#5C9CFF] font-bold transition delay-50">Sponsors</a>
-        <a href="#" className="text-gray-100 mr-7 hover:text-[#5C9CFF] font-bold transition delay-50">Schedule</a>
-        <a href="#" className="text-gray-100 mr-7 hover:text-[#5C9CFF] font-bold transition delay-50">Media</a>
-        <a href="#" className="text-gray-100 mr-7 hover:text-[#5C9CFF] font-bold transition delay-50">Contact Us</a>
+        <a href="#" className="text-gray-100 mr-9 hover:text-hov font-semibold transition delay-50">About</a>
+        <a href="#" className="text-gray-100 mr-9 hover:text-hov font-semibold transition delay-50">Sponsors</a>
+        <a href="#" className="text-gray-100 mr-9 hover:text-hov font-semibold transition delay-50">Schedule</a>
+        <a href="#" className="text-gray-100 mr-9 hover:text-hov font-semibold transition delay-50">Media</a>
+        <a href="#" className="text-gray-100 mr-9 hover:text-hov font-semibold transition delay-50">Contact Us</a>
       </nav>
     </header>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,25 @@
 import { Inter } from "next/font/google";
 import Navbar from "@/components/navbar";
-import {Footer} from "@/components/footer";
-import Typewriter from "typewriter-effect";
+import { Footer } from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    
-    <main className={`min-h-screen flex-col items-center justify-between p-[25px] bg-black ${inter.className}`}>
+    <main className={`min-h-screen flex flex-col items-center justify-between bg-black p-[25px] ${inter.className}`}>
       <Navbar />
 
-        <Footer />
+      <div className="text-center">
+        <h1 className="text-hov font-black text-8xl md:text-[120px]">
+          FutureMD{" "}
+          <span className="text-white">Inc</span>
+        </h1>
+        <p className="text-lg md:text-3xl font-medium mt-5 text-gray-400">
+          A student-led nonprofit organization
+        </p>
+      </div>
+
+      <Footer />
     </main>
   );
 }
