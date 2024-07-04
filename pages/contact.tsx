d892c29d288da5a53a709b7acc59dd6c8285590d
Advay Chandorkar <advay.chandorkar@gmail.com>
2024-07-04 00:48:25 -0400

    try making responsive (menu on nav to do later)

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -8,22 +8,22 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Contact() {
   return (
-    <main className={`min-h-screen flex-col items-center justify-between pt-[25px] ${inter.className} dark:bg-black bg-light-bg`}>
+    <main className={`min-h-screen flex flex-col items-center justify-between py-8 ${inter.className} dark:bg-black bg-light-bg`}>
       <Navbar />
-      <div className="pt-[10px]">
-        <h1 className="text-6xl font-bold my-8 mb-4 text-center dark:text-white text-black">Contact Us</h1>
-        <p className="text-center mb-1 text-2xl font-bold dark:text-dark-text text-gray-600">
-          If you have a question, or business inquiry, <br />feel free to contact us here to get a quick response!
+      <div className="py-8 px-4 w-full max-w-9xl">
+        <h1 className="text-4xl md:text-6xl font-bold my-8 mb-4 text-center dark:text-white text-black">Contact Us</h1>
+        <p className="text-center mb-4 text-lg md:text-2xl font-bold dark:text-dark-text text-gray-600">
+          If you have a question or business inquiry, <br />feel free to contact us here to get a quick response!
         </p>
         <ContactForm />
-        <div className="flex flex-col items-center mt-6 p-8">
-          <h2 className="text-4xl font-bold mb-4 dark:text-white text-black">Connect With Us</h2>
+        <div className="flex flex-col items-center mt-6">
+          <h2 className="text-3xl md:text-4xl font-bold mb-4 dark:text-white text-black">Connect With Us</h2>
           <div className="flex space-x-4">
             <a href="https://www.instagram.com/futuremd_team/" target="_blank" rel="noopener noreferrer">
-              <FaInstagram size={40} className="text-hov hover:text-primary transition-all duration-300" />
+              <FaInstagram size={32} className="text-hov hover:text-primary transition-all duration-300" />
             </a>
             <a href="https://www.linkedin.com/company/futuremdteam/" target="_blank" rel="noopener noreferrer">
-              <FaLinkedin size={40} className="text-hov hover:text-primary transition-all duration-300" />
+              <FaLinkedin size={32} className="text-hov hover:text-primary transition-all duration-300" />
             </a>
           </div>
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,15 +6,15 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={`min-h-screen flex flex-col items-center justify-between p-[25px] ${inter.className} dark:bg-dark-bg bg-light-bg`}>
+    <main className={`min-h-screen flex flex-col items-center justify-between p-4 md:p-8 ${inter.className} dark:bg-dark-bg bg-light-bg`}>
       <Navbar />
-      <div className="text-center">
-        <h1 className="font-black dark:text-hov text-[#3C55B7] text-8xl md:text-[120px]">
+      <div className="text-center px-4 md:px-8">
+        <h1 className="font-black dark:text-hov text-[#3C55B7] text-4xl md:text-8xl">
           FutureMD{" "}
           <span className="dark:text-white text-black">Inc</span>
         </h1>
-        <p className="text-lg md:text-3xl font-bold mt-5 dark:text-dark-text text-gray-600">
-        A student-led nonprofit organization with the goal to educate <br></br> teens about life during & after medical school!
+        <p className="text-lg md:text-3xl font-bold mt-4 md:mt-8 dark:text-dark-text text-gray-600">
+          A student-led nonprofit organization with the goal to educate teens about life during & after medical school!
         </p>
       </div>
       <Footer />
