6be9c96e5eb4626466817ce899431fa6c47adb4f
Bobbyf0814 <155799880+Bobbyf0814@users.noreply.github.com>
2024-07-05 22:26:14 -0400

    Update team.tsx

--- pages/team.tsx
+++ pages/team.tsx
@@ -9,7 +9,7 @@ export default function About() {
     <main className={`min-h-screen items-center justify-between py-8 ${inter.className} dark:bg-dark-bg bg-light-bg`}>
       <Navbar />
       <header className="pt-8 pd-3 px-4 w-full max-w-9xl">
-        <h1 className="text-4xl md:text-6xl font-bold my-8 mb-4 text-center dark:text-white text-black"> About Us</h1>
+        <h1 className="text-4xl md:text-6xl font-bold my-8 mb-4 text-center dark:text-white text-black"> Our Team</h1>
         <p className="text-center mb-4 text-lg md:text-2xl font-semibold dark:text-dark-text text-[#828282]">
           Discover The Team behind FutureMD!
         </p>
