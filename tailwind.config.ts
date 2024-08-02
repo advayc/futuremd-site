c63115486124872e0e751048750e1265299aef8f
Bobbyf0814 <155799880+Bobbyf0814@users.noreply.github.com>
2024-08-02 15:50:38 -0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
     extend: {
       colors: {
         primary: "#D9E8FF",
-        dprimary: "##1f1e1e",
+        dprimary: "#171616",
         hov: "#5C9CFF",
         navy: "#28387D",
         li: "#3C55B7",
