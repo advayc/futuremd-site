214286fa0f6ef6974760143a485a31bb13a9cd37
Bobbyf0814 <155799880+Bobbyf0814@users.noreply.github.com>
2024-08-01 22:49:48 -0400

    Update guest_speakers.ts

--- lib/guest_speakers.ts
+++ lib/guest_speakers.ts
@@ -1,20 +1,20 @@
 export const guest_speakers = [
     {
-      title: "Person 1",
+      title: "???",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
       image: "logo.png",
       linkedin: "https://www.linkedin.com/company/futuremdteam/mycompany/",
       instagram: "https://www.instagram.com/futuremd_team/",
     },
     {
-        title: "Person 2",
+        title: "???",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
         image: "logo.png",
         linkedin: "https://www.linkedin.com/company/futuremdteam/mycompany/",
         instagram: "https://www.instagram.com/futuremd_team/",
       },
     {
-      title: "Person 3",
+      title: "???",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
       image: "logo.png",
       linkedin: "https://www.linkedin.com/company/futuremdteam/mycompany/",
