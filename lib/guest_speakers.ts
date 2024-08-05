82e56cac987ebcf38015698bba20a673c505e6cc
Bobbyf0814 <155799880+Bobbyf0814@users.noreply.github.com>
2024-08-04 20:10:18 -0400

    Update guest_speakers.ts

--- lib/guest_speakers.ts
+++ lib/guest_speakers.ts
@@ -1,21 +1,21 @@
 export const guest_speakers = [
     {
       title: "???",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      description: "Check back soon!",
       image: "logo.png",
       linkedin: "https://www.linkedin.com/company/futuremdteam/mycompany/",
       instagram: "https://www.instagram.com/futuremd_team/",
     },
     {
         title: "???",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        description: "Check back soon!",
         image: "logo.png",
         linkedin: "https://www.linkedin.com/company/futuremdteam/mycompany/",
         instagram: "https://www.instagram.com/futuremd_team/",
       },
     {
       title: "???",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      description: "Check back soon!",
       image: "logo.png",
       linkedin: "https://www.linkedin.com/company/futuremdteam/mycompany/",
       instagram: "https://www.instagram.com/futuremd_team/",
