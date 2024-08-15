b70fe81025e1e5847998fb5096127125bd8da785
Bobbyf0814 <155799880+Bobbyf0814@users.noreply.github.com>
2024-08-15 16:13:16 -0400

    Update team.ts

--- lib/team.ts
+++ lib/team.ts
@@ -62,9 +62,9 @@ export const team = [
     },
     {
       title: "Sameer F.",
-      role: "Design",
+      role: "Finance",
       description: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available.",
-      image: "logo.jpg",
+      image: "logo.png",
       instagram: "https://www.instagram.com/fijibutter",
     },    
 ];
