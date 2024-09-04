35e0ea5bb9b86792672444005a844f8a7e7e0b58
Advay Chandorkar <advay.chandorkar@gmail.com>
2024-09-04 02:15:20 -0400

    lm

--- pages/blogs/[slug].tsx
+++ pages/blogs/[slug].tsx
@@ -1,3 +1,4 @@
+import { Inter } from "next/font/google";
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
@@ -5,6 +6,8 @@ import { Footer } from '@/components/footer';
 import Navbar from '@/components/navbar';
 import { blogPosts, BlogPost } from '@/pages/blog';
 import { format, parseISO } from 'date-fns';
+
+const inter = Inter({ subsets: ["latin"] });
 
 const BlogPostPage = ({ post }: { post: BlogPost }) => {
   const router = useRouter();
@@ -14,41 +17,41 @@ const BlogPostPage = ({ post }: { post: BlogPost }) => {
   }
 
   return (
-    <main className="min-h-screen pt-8 dark:bg-dark-bg bg-light-bg transition-colors duration-700">
+    <main className={`${inter.className} min-h-screen pt-8 dark:bg-dark-bg bg-light-bg transition-colors duration-700`}>
       <Head><title>{post.title} - FutureMD</title></Head>
       <Navbar showAnimation={false} />
-      <header className="pt-4 px-4 w-full max-w-7xl mx-auto text-center relative">
+      <header className="pt-4 px-36 w-full max-w-7xl mx-auto text-center relative">
         <button
-          onClick={() => router.push('/blogs')}
-          className="absolute top-4 left-4 text-dark-text hover:text-default-900 transition-colors"
+          onClick={() => router.push('/blog')}
+          className="absolute top-4 left-20 text-dark-text hover:text-default-900 transition-colors"
         >
           &#60; Back to Blog
         </button>
 
         <h1 className="text-4xl md:text-6xl font-bold my-8 mb-4 text-center dark:text-white text-black">{post.title}</h1>
-        <h2 className="text-center text-lg md:text-xl font-semibold dark:text-dark-text text-dark-text">
+        <h2 className="text-center text-lg md:text-xl font-semibold dark:text-dark-text text-dark-text mb-8">
           {post.description}
         </h2>
         <time className="block text-left mb-3 font-semibold text-dark-text" dateTime={post.date}>
           {format(parseISO(post.date), "LLLL d, yyyy")}
         </time>
         <div className="mb-8 flex w-full flex-col items-start">
-          <div className="flex items-center space-x-3 rounded rounded-lg cursor-pointer transition-colors px-4 py-1.5 hover:bg-default-100 dark:hover:bg-default-50 transition-colors -ml-4">
+          <div className="flex items-center space-x-3 rounded rounded-lg cursor-pointer transition-colors px-4 py-1.5 hover:bg-zinc-300 dark:hover:bg-default-50 transition-colors -ml-4">
             <img className="w-11 h-11 rounded-full" src={post.author.avatar} alt={post.author.name} />
             <div>
-              <p className="text-gray-200 font-semibold">{post.author.name}</p>
-              <a href={post.author.link} className="text-zinc-500 text-sm hover:text-hov transition-colors">
+              <p className="dark:text-gray-200 text-gray-900 font-semibold">{post.author.name}</p>
+              <a href={post.author.link} className="text-zinc-500 text-sm dark:hover:text-hov hover:text-li transition-colors">
                 @{post.author.user}
               </a>
             </div>
           </div>
         </div>
       </header>
-      <section className="px-4">
+      <div className="px-36 max-w-7xl mx-auto">
         <img className="mb-4 w-full h-80 object-cover rounded-md" src={post.image} alt={post.title} />
         <p className="text-dark-text">{post.description}</p>
-        <Footer />
-      </section>
+      </div>
+    <Footer />
     </main>
   );
 };
