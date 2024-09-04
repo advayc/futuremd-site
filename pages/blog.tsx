f95fffd195597c49e2822c747dfc7c7d0b67fa7b
Advay Chandorkar <advay.chandorkar@gmail.com>
2024-09-04 00:45:25 -0400

    centered each

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -9,8 +9,10 @@ import { motion, AnimatePresence } from "framer-motion";
 const inter = Inter({ subsets: ["latin"] });
 
 const BlogPostCard = ({ title, url, image, description, date, author }: BlogPost) => {
+  const router = useRouter();
+
   const handlePress = () => {
-    console.log("Blog post clicked:", title);
+    router.push(url);
   };
 
   return (
@@ -20,26 +22,26 @@ const BlogPostCard = ({ title, url, image, description, date, author }: BlogPost
         exit={{ opacity: 0, y: 5 }}
         initial={{ opacity: 0, y: 5 }}
         transition={{ duration: 0.3 }}
+        whileHover={{ scale: 1.05, transition: { duration: 0.3 } }}
+        whileTap={{ scale: 0.95 }}
       >
         <div
-          className="p-2 h-full border-transparent text-start bg-white/5 dark:bg-default-400/10 backdrop-blur-lg backdrop-saturate-[1.8]"
+          className="justify-center items-center p-4 h-full border-transparent text-start dark:bg-default-400/10 rounded-lg shadow-lg cursor-pointer"
           onClick={handlePress}
         >
-          <header>
             <a
               href={url}
-              className="font-semibold"
-              onClick={handlePress}
+              className="ml-2 text-xl text-li dark:text-hov font-bold hover:underline"
+              onClick={(e) => e.preventDefault()}
             >
               {title}
-            </a>
-          </header>
-          <div className="pt-0 px-2 pb-1">
-            <img className="mb-4" src={image} alt={title} />
-            <p className="font-normal w-full text-default-600">{description}</p>
+          </a>
+          <div className="p-2 mt-1">
+            <img className="mb-4 w-full h-40 object-cover rounded-md" src={image} alt={title} />
+            <p className="font-bold w-full text-dark-text">{description}</p>
           </div>
-          <footer className="flex justify-between items-center">
-            <time className="block text-small text-default-500" dateTime={date}>
+          <footer className="flex justify-between items-center mt-2">
+            <time className="ml-2 block text-small text-dark-text" dateTime={date}>
               {new Date(date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}
             </time>
             <img className="w-10 h-10 rounded-full" src={author.avatar} alt="Author" />
@@ -61,12 +63,11 @@ type BlogPost = {
   };
 };
 
-
 const blogPosts: BlogPost[] = [
   {
     title: "Welcome to FutureMD!",
     url: "/blog/welcome",
-    image: "/images/welcome-to-futuremd.jpg",
+    image: "/events/path2medc.png",
     description: "We are excited to launch FutureMD! Stay tuned for more updates.",
     date: "2024-05-24T00:00:00Z",
     author: {
@@ -76,7 +77,7 @@ const blogPosts: BlogPost[] = [
   {
     title: "Join us to Path2Med",
     url: "/blog/join-us-to-path2med",
-    image: "/images/join-us-to-path2med.jpg",
+    image: "/events/path2med.png",
     description: "Discover Path2Med and how you can be a part of it.",
     date: "2024-07-28T00:00:00Z",
     author: {
@@ -87,10 +88,12 @@ const blogPosts: BlogPost[] = [
 
 export const BlogPostList = ({ posts }: { posts: BlogPost[] }) => {
   return (
-    <div className="mt-10 grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
-      {posts.map((post, idx) => (
-        <BlogPostCard key={idx} {...post} />
-      ))}
+    <div className=" flex justify-center">
+      <div className="mb-20 mt-2 grid gap-4 grid-cols-[repeat(auto-fit,minmax(300px,1fr))] max-w-3xl">
+        {posts.map((post, idx) => (
+          <BlogPostCard key={idx} {...post} />
+        ))}
+      </div>
     </div>
   );
 };
@@ -117,7 +120,7 @@ const Blog: React.FC = () => {
     <main className={`min-h-screen pt-8 ${inter.className} dark:bg-dark-bg bg-light-bg transition-colors duration-700`}>
       <Head><title>FutureMD - Blog</title></Head>
       <Navbar showAnimation={false} />
-      <header className="pt-4 pd-3 px-4 w-full max-w-9xl text-center">
+      <header className="pt-4 pd-3 px-4 w-full max-w-7xl mx-auto text-center">
         <h1 className="text-4xl md:text-6xl font-bold my-8 mb-4 text-center dark:text-white text-black">Blog</h1>
         <h2 className="text-center text-lg md:text-xl font-semibold dark:text-dark-text text-dark-text mb-8 ">
           All the latest news about FutureMD!
