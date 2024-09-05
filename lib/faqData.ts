4cce1a840cd988df8409c004e86094f42183b8f6
Bobbyf0814 <155799880+Bobbyf0814@users.noreply.github.com>
2024-09-04 21:02:13 -0400

    Update faqData.ts

--- lib/faqData.ts
+++ lib/faqData.ts
@@ -3,7 +3,7 @@ export const faqData = {
     {
       question: "Where are FutureMD events located, and how can I attend?",
       answer:
-        "FutureMD events are primarily located at UTM's William G. Davis Facility (1867 Inner Cir Rd, Mississauga, ON L5L 1C6). However, sometimes we may change our venue. To verify our location for the next event, please click <a class='hyperlink' href='/events' target='_blank' >here</a> to see the list of our events!",
+        "FutureMD events are primarily located at <a class='hyperlink' href='https://www.utm.utoronto.ca/facilities/buildings/william-g-davis-building' target='_blank' >UTM's William G. Davis Facility</a> (1867 Inner Cir Rd, Mississauga, ON L5L 1C6). However, sometimes we may change our venue. To verify our location for the next event, please click on the inquired event <a class='hyperlink' href='/events' target='_blank' >here</a>.",
     },
     {
       question:
@@ -14,7 +14,7 @@ export const faqData = {
     {
       question: "How can I get involved with FutureMD as a volunteer or guest speaker?",
       answer:
-        "There are a variety of ways you can get in touch with us! We are <strong>always</strong> looking to expand our team! If you are interested in joining our team or volunteering with us, click <a class='hyperlink' href='/contact' target='_blank' >here</a>. If you have experience and wish to be a guest speaker, please click <a class='hyperlink' href='/contact' target='_blank' >here</a>.",
+        "There are a variety of ways you can get in touch with us! We are <strong>always</strong> looking to expand our team! If you are interested in joining our team or volunteering with us, click <a class='hyperlink' href='/apply' target='_blank' >here</a>. If you have experience and wish to be a guest speaker, please click <a class='hyperlink' href='/contact' target='_blank' >here</a>.",
     },
     {
       question: "Is there a cost to attend FutureMD events, or are they free?",
@@ -22,13 +22,13 @@ export const faqData = {
         "All FutureMD events are <strong>FREE</strong>! There is absolutely no cost to attend whatsoever! We believe in giving all students & youth access to medical knowledge free of charge, and we hope to empower the next generation of medical specialists!",
     },
     {
-      question: "What age group are your events geared towards, and do you offer programs for different levels of interest in medicine?",
-      answer: "Our events are primarily targeted at teens and youth aged 12-18.",
+      question: "What age group are your events geared towards?",
+      answer: "Our events are primarily targeted at teens & youth aged 12-18. We do, however, allow youth younger than 12 to participate in our events as long as they <a class='hyperlink' href='https://futuremd.net/register' target='_blank' >register</a>.",
     },
     {
       question: "How can I stay updated on upcoming FutureMD events and news?",
       answer:
-        "You can check us out on any of our social media platforms! You can find us on <a class='hyperlink' href='https://tiktok.com/@futuremd_team' target='_blank' >TikTok</a>, <a class='hyperlink' href='https://instagram.com/futuremd_team' target='_blank' >Instagram</a>, <a class='hyperlink' href='https://www.linkedin.com/company/futuremdteam/' target='_blank' >LinkedIn</a>, and more! Additionally, you can also subscribe to our monthly <a class='hyperlink' href='/newsletter' target='_blank' >newsletter</a> featuring sneak peeks, exclusive information, and much more!",
+        "You can check us out on any of our social media platforms! You can find us on <a class='hyperlink' href='https://tiktok.com/@futuremd_team' target='_blank' >TikTok</a>, <a class='hyperlink' href='https://instagram.com/futuremd_team' target='_blank' >Instagram</a>, <a class='hyperlink' href='https://www.linkedin.com/company/futuremdteam/' target='_blank' >LinkedIn</a>, and more! Additionally, you can also subscribe to our monthly <a class='hyperlink' href='/newsletter' target='_blank' >newsletter</a> featuring sneak peeks & exclusive information, or check out our <a class='hyperlink' href='/blog' target='_blank' >blogs</a> featuring important updates!",
     },
   ],
 };
