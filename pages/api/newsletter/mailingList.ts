b877d1bfb807f0af07462f5ed001ac26b9368ed3
Advay Chandorkar <advay.chandorkar@gmail.com>
2024-09-22 21:17:03 -0400

    this guy

---
+++ pages/api/newsletter/mailingList.ts
@@ -0,0 +1,41 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  message: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const email = req.body.mail;
+  const url = `https://api.sendgrid.com/v3/marketing/contacts`;
+
+  const data = {
+    contacts: [{ email: email }],
+    list_ids: [process.env.SENDGRID_MAILING_ID],
+  };
+  const headers = {
+    Authorization: `Bearer ${process.env.SENDGRID_API_KEY}`,
+    "Content-Type": "application/json",
+  };
+
+  const options = {
+    method: "PUT",
+    headers: headers,
+    body: JSON.stringify(data),
+  };
+  const response = await fetch(url, options);
+  const json = await response.json();
+  if (json.errors) {
+    res.status(500).json({
+      message:
+        "Oups, there was a problem with your subscription, please try again or contact us",
+    });
+  } else {
+    res.status(200).json({
+      message:
+        "Your email has been succesfully added to the mailing list. Welcome 👋",
+    });
+  }
+}
--- pages/newsletter.tsx
+++ pages/newsletter.tsx
@@ -1,65 +1,136 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/navbar";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
-import Head from 'next/head'; 
-import { Footer } from '@/components/footer';
-
+import Head from "next/head";
+import { Footer } from "@/components/footer";
+import { RiLoader5Fill } from "react-icons/ri";
+import { FaCheck } from "react-icons/fa"; 
+import { IoMdClose } from "react-icons/io";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Events() {
   const router = useRouter();
 
+  const [mail, setMail] = useState<any | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const [messageState, setMessageState] = useState<string>("");
+
   useEffect(() => {
     const handleRouteChange = (url: string) => {
-      document.documentElement.classList.add('transition-colors', 'duration-700');
+      document.documentElement.classList.add("transition-colors", "duration-700");
       setTimeout(() => {
-        document.documentElement.classList.remove('transition-colors', 'duration-700');
+        document.documentElement.classList.remove("transition-colors", "duration-700");
       }, 1700);
     };
 
-    router.events.on('routeChangeStart', handleRouteChange);
+    router.events.on("routeChangeStart", handleRouteChange);
 
     return () => {
-      router.events.off('routeChangeStart', handleRouteChange);
+      router.events.off("routeChangeStart", handleRouteChange);
     };
   }, [router.events]);
 
+  const Subscribe = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    setSuccess(false);
+    setErrorMessage("");
+
+    axios
+      .put("/api/newsletter/mailingList", {
+        mail: mail,
+      })
+      .then((res) => {
+        if (res.status === 200) {
+          setLoading(false);
+          setSuccess(true);
+          setMessageState(res.data.message);
+        } else {
+          setLoading(false);
+          setErrorMessage(res.data.message);
+        }
+      })
+      .catch((err) => {
+        setLoading(false);
+        setErrorMessage(String(err.message));
+      });
+  };
+
+  const closeMessage = () => {
+    setSuccess(false);
+    setMessageState("");
+  };
+
   return (
-    <main className={`min-h-screen flex flex-col justify-center pt-8 pb-20 ${inter.className} dark:bg-dark-bg bg-light-bg transition-colors duration-700`}>
-      <Head><title>FutureMD - Newsletter</title></Head>
+    <main className={`min-h-screen flex flex-col justify-center pt-8 ${inter.className} dark:bg-dark-bg bg-light-bg transition-colors duration-700`}>
+      <Head>
+        <title>FutureMD - Newsletter</title>
+      </Head>
       <Navbar showAnimation={false} />
       <header className="pt-4 px-2 w-full max-w-5xl mx-auto">
         <div className="md:flex items-start justify-between mb-20">
-          <div className="md:w-2/5 mt-16"> 
+          <div className="md:w-3/5 mt-16">
             <h1 className="text-2xl md:text-4xl font-bold mt-16 mb-5 dark:text-white text-black whitespace-nowrap text-left">
-              Subscribe To The <span className="dark:text-hov text-li">Newsletter</span>
+              Subscribe To The <span className="dark:text-hov text-blue-600">Newsletter</span>
             </h1>
             <p className="text-left text-md md:text-lg font-semibold dark:text-dark-text text-[#828282] mb-8">
-              Receive monthly updates on <span className="dark:text-hov text-li">FutureMD</span> & our events!
+              Receive monthly updates on <span className="dark:text-hov text-blue-600">FutureMD</span> & our events!
             </p>
-            <form className="flex justify-start mb-16">
+            <form onSubmit={Subscribe} className="flex justify-start mb-16">
               <input
                 type="email"
                 placeholder="Email Address"
-                className="p-2 border border-gray-300 rounded-lg w-full md:w-auto"
+                className="p-2 pr-4 pl-2 border border-gray-300 rounded-lg w-full md:w-auto"
+                required
+                onChange={(e) => setMail(e.target.value)}
               />
               <button
+                disabled={loading}
                 type="submit"
-                className="ml-2 p-2 bg-[#2E3B85] text-[#E4EBFF] rounded-lg hover:bg-[#243171] transition-colors duration-300"
+                className="ml-2 px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-lg hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
               >
-                Subscribe
+                {!loading ? (
+                  "Subscribe"
+                ) : (
+                  <div className="flex w-full items-center justify-center">
+                    <RiLoader5Fill className="w-8 animate-spin" />
+                  </div>
+                )}
               </button>
             </form>
           </div>
-          <img src="/newsletter.png" alt="Newsletter" className="w-80 md:w-2/3 h-auto" /> 
+          <img src="/newsletter.png" alt="Newsletter" className="imghov w-80 md:w-3/4  h-auto" />
         </div>
       </header>
+
+      {success && (
+        <div
+          id="success-message"
+          className="fixed bottom-4 left-4 w-full max-w-md p-4 bg-green-500 text-white rounded-md shadow-lg flex items-center justify-between transition-opacity duration-1000"
+          style={{ opacity: 1 }}
+        >
+          <FaCheck className="mr-2" size={20} />
+          <span>{messageState}</span>
+          <button onClick={closeMessage} className="text-white">
+            <IoMdClose size={24} />
+          </button>
+        </div>
+      )}
+
+      {errorMessage && (
+        <div className="fixed bottom-4 left-4 w-full max-w-md p-4 bg-red-500 text-white rounded-md shadow-lg flex items-center justify-between">
+          <span>{errorMessage}</span>
+          <button onClick={() => setErrorMessage("")} className="text-white">
+            <IoMdClose size={24} />
+          </button>
+        </div>
+      )}
       <Footer />
     </main>
   );
-  
 }
-
