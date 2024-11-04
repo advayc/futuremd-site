b61bcb01b61d4d45ece721bc6ec9713909668c92
Advay Chandorkar <advay.chandorkar@gmail.com>
2024-11-03 23:50:31 -0500

    uh

--- lib/guest_speakers.ts
+++ lib/guest_speakers.ts
@@ -2,8 +2,9 @@ export const guest_speakers = [
     {
       title: "Haris Saud",
       description: "Haris Saud is a physician who works in the Halton and Peel area, with his practice including hospitalist medicine, palliative care, and surgical assisting. He was raised in Mississauga and did his postsecondary and medical training at McMaster University and the University of Toronto.",
-      image: "/public/team/haris.jpg",
+      image: "/team/haris.jpg",
       linkedin: "https://www.linkedin.com/in/harissaud/",
+      instagram: "https://www.instagram.com/futuremd_team/",
     },
     {
         title: "???",
