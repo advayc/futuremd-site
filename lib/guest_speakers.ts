3d2153e50bc42d498a4b10e2fb6aee4473e26782
Bobbyf0814 <155799880+Bobbyf0814@users.noreply.github.com>
2024-11-03 23:22:53 -0500

    Update guest_speakers.ts

--- lib/guest_speakers.ts
+++ lib/guest_speakers.ts
@@ -1,10 +1,9 @@
 export const guest_speakers = [
     {
-      title: "???",
-      description: "Check back soon!",
-      image: "logo.png",
-      linkedin: "https://www.linkedin.com/company/futuremdteam/mycompany/",
-      instagram: "https://www.instagram.com/futuremd_team/",
+      title: "Haris Saud",
+      description: "Haris Saud is a physician who works in the Halton and Peel area, with his practice including hospitalist medicine, palliative care, and surgical assisting. He was raised in Mississauga and did his postsecondary and medical training at McMaster University and the University of Toronto.",
+      image: "haris.jpg",
+      linkedin: "https://www.linkedin.com/in/harissaud/",
     },
     {
         title: "???",
