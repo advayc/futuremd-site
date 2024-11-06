8db0dc454c061f40dc2918616b29f4986bf591c6
Bobbyf0814 <155799880+Bobbyf0814@users.noreply.github.com>
2024-11-06 17:24:33 -0500

    Update guest_speakers.ts

--- lib/guest_speakers.ts
+++ lib/guest_speakers.ts
@@ -7,9 +7,9 @@ export const guest_speakers = [
       instagram: "https://www.instagram.com/futuremd_team/",
     },
     {
-        title: "???",
-        description: "Check back soon!",
-        image: "logo.png",
+        title: "Fatima Bah",
+        description: "Fatima Bah is a third-year medical student at the University of Toronto. She completed her undergraduate studies in Life Sciences at Ontario Tech University (UOIT). While at UOIT, she conducted research in the Aquatic Omics Lab, focusing on proteomics and genomics, which led to a publication in “Frontiers.” In addition to her academic pursuits, Fatima is interested in women’s health and co-founded the non-profit organization Wellness of Women, which aims to empower women and support local shelters. She is also passionate about learner wellness and equity and diversity initiatives. Outside of her professional life, she enjoys travelling and engaging in outdoor activities such as camping and hiking!",
+        image: "/team/fatima.jpg",
         linkedin: "https://www.linkedin.com/company/futuremdteam/mycompany/",
         instagram: "https://www.instagram.com/futuremd_team/",
       },
