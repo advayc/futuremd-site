7676f02670befe9bff363dd5cc28ae4b9610c56a
Advay Chandorkar <advay.chandorkar@gmail.com>
2024-11-14 23:22:52 -0500

    gt

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Custom404 = () => {
   return (
@@ -13,13 +14,13 @@ const Custom404 = () => {
           <h1 className="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">Page not found</h1>
           <p className="mt-4 text-gray-500 dark:text-gray-400">The page you are looking for doesn't exist</p>
 
-            <button className="mt-4 w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-li rounded-lg shrink-0 sm:w-auto imghov">
-              <a href="/">
+          <Link href="/">
+            <button className="mt-4 w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-li rounded-lg shrink-0 sm:w-auto">
               Take me home
-              </a>
             </button>
-          </div>
+          </Link>
         </div>
+      </div>
     </section>
   );
 };
